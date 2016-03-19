1324502dd25afbb16524ac21117dd45b47286001
kadoufall <624938994@qq.com>
2016-03-19 12:58:38 +0800

    Nothing Important

--- Lab1/src/lab1/MainTest.java
+++ Lab1/src/lab1/MainTest.java
@@ -26,25 +26,14 @@ public class MainTest {
 		
 		
 		
-		String[] str ={"2","houseblend","small","whip","cream","milk",";","houseblend","whip","cream","milk"};
+		String[] str ={"houseblend","small","whip","milk"};
 		Main.main(str);
 		String strMsg = baoStream.toString().trim();
 		System.setOut(oldStream);
 	//	System.out.println(strMsg);
 		Assert.assertEquals("The total cost of your order is: 2.0", strMsg);
-/*		
-		baoStream.reset();
-		cacheStream = new PrintStream(baoStream);
-		oldStream = System.out;
-		System.setOut(cacheStream);
-*/	/*	
-		str[1] = "small";
-		Main.main(str);
-		strMsg = baoStream.toString().trim();
-		System.setOut(oldStream);
-	//	System.out.println(strMsg);
-		Assert.assertEquals("The total cost of your order is: 2.1", strMsg);
-	*/	
+
+		
 	}
 
 }
