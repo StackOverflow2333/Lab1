21ce51d327c847f4b8ab65be85905be5c7a78c60
kadoufall <624938994@qq.com>
2016-03-19 11:24:08 +0800

    MainTest修改

--- Lab1/src/lab1/Main.java
+++ Lab1/src/lab1/Main.java
@@ -1,6 +1,7 @@
 package lab1;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) {
@@ -8,93 +9,109 @@ public class Main {
 		for (int j = 0; j < args.length; j++) {
 			disArr[j] = args[j].toLowerCase();
 		}
-//判断商品名字为几位
-		int i;
-		for (i = 0; i < disArr.length; i++)
-			if (disArr[i].equals("small") || disArr[i].equals("medium")
-					|| disArr[i].equals("large") || disArr[i].equals("grand"))  //增加
-				break;
-
-		if (i >= disArr.length) {
-			System.out.println("Must set a size!");
-			return;
-		}
-
+		
 		String beveStr;
-		if (i == 2) {
-			beveStr = disArr[0] + " " + disArr[1];
-		} else {
-			beveStr = disArr[0];
-		}
+		Beverage order = new Beverage();
 
-		Beverage order;
-		if (beveStr.equals("espresso")) {
-			order = new CoffeeBeverage();
-			order = new Espresso();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-		} else if (beveStr.equals("houseblend")) {
-			order = new CoffeeBeverage();
-			order = new HouseBlend();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-		} else if (beveStr.equals("mocha")) {
-			order = new Espresso();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-			order = new Chocolate(order);
-		} else if (beveStr.equals("latte")) {
-			order = new Espresso();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-			order = new Milk(order);
-		} else if (beveStr.equals("cappuccino")) {
-			order = new Espresso();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-			order = new WhipCream(order);
-		} else if (beveStr.equals("decaf mocha")) {			//增加
-			order = new Decaf();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-			order = new Chocolate(order);
-		} else if (beveStr.equals("green tea")) {
-			order = new GreenTea();
-			((TeaBeverage) order).setSize(disArr[i]);
-		} else if (beveStr.equals("red tea")) {
-			order = new RedTea();
-			((TeaBeverage) order).setSize(disArr[i]);
-		} else if (beveStr.equals("white tea")) {
-			order = new WhiteTea();
-			((TeaBeverage) order).setSize(disArr[i]);
-		} else if (beveStr.equals("flower tea")) {
-			order = new GreenTea();
-			((TeaBeverage) order).setSize(disArr[i]);
-			order = new Jasmine(order);
-		} else if (beveStr.equals("ginger tea")) {
-			order = new GreenTea();
-			((TeaBeverage) order).setSize(disArr[i]);
-			order = new Ginger(order);
-		} else if (beveStr.equals("tea latte")) {
-			order = new RedTea();
-			((TeaBeverage) order).setSize(disArr[i]);
-			order = new Milk(order);
-		} else {
-			System.out.println("Illegal input: " + beveStr);
-			return;
-		}
+			//判断商品名字为几位
+			int i;
+			for (i = 0; i < disArr.length; i++)
+				if (disArr[i].equals("small") || disArr[i].equals("medium")
+						|| disArr[i].equals("large") || disArr[i].equals("grand"))  //增加
+					break;
 
-		for (i++; i < disArr.length; i++) {
-			if (disArr[i].equals("chocolate")) {
-				order = new Chocolate(order);
-			} else if (disArr[i].equals("ginger")) {
-				order = new Ginger(order);
-			} else if (disArr[i].equals("milk")) {
-				order = new Milk(order);
-			} else if (disArr[i].equals("jasmine")) {
-				order = new Jasmine(order);
-			} else if (disArr[i].equals("whip")) {
-				i++;
-				order = new WhipCream(order);
-			} else {
-				System.out.println("Illegal input: " + disArr[i]);
+			if (i >= disArr.length) {
+				System.out.println("Must set a size!");
+				return;
 			}
-		}
 
+			
+			if (i == 2) {
+				beveStr = disArr[0] + " " + disArr[1];
+			} else {
+				beveStr = disArr[0];
+			}
+
+			
+			if (beveStr.equals("espresso")) {
+				order = new CoffeeBeverage();
+				order = new Espresso();
+				((CoffeeBeverage) order).setSize(disArr[i]);
+			} else if (beveStr.equals("houseblend")) {
+				order = new CoffeeBeverage();
+				order = new HouseBlend();
+				((CoffeeBeverage) order).setSize(disArr[i]);
+			} else if (beveStr.equals("mocha")) {
+				order = new Espresso();
+				((CoffeeBeverage) order).setSize(disArr[i]);
+				order = new Chocolate(order);
+			} else if (beveStr.equals("latte")) {
+				order = new Espresso();
+				((CoffeeBeverage) order).setSize(disArr[i]);
+				order = new Milk(order);
+			} else if (beveStr.equals("cappuccino")) {
+				order = new Espresso();
+				((CoffeeBeverage) order).setSize(disArr[i]);
+				order = new WhipCream(order);
+			} else if (beveStr.equals("decaf mocha")) {			//增加
+				order = new Decaf();
+				((CoffeeBeverage) order).setSize(disArr[i]);
+				order = new Chocolate(order);
+			} else if (beveStr.equals("decaf latte")) {			//增加
+				order = new Decaf();
+				((CoffeeBeverage) order).setSize(disArr[i]);
+				order = new Milk(order);
+			} else if (beveStr.equals("decaf cappuccino")) {	//增加
+				order = new Decaf();
+				((CoffeeBeverage) order).setSize(disArr[i]);
+				order = new WhipCream(order);
+			} else if (beveStr.equals("green tea")) {
+				order = new GreenTea();
+				((TeaBeverage) order).setSize(disArr[i]);
+			} else if (beveStr.equals("red tea")) {
+				order = new RedTea();
+				((TeaBeverage) order).setSize(disArr[i]);
+			} else if (beveStr.equals("white tea")) {
+				order = new WhiteTea();
+				((TeaBeverage) order).setSize(disArr[i]);
+			} else if (beveStr.equals("flower tea")) {
+				order = new GreenTea();
+				((TeaBeverage) order).setSize(disArr[i]);
+				order = new Jasmine(order);
+			} else if (beveStr.equals("ginger tea")) {
+				order = new GreenTea();
+				((TeaBeverage) order).setSize(disArr[i]);
+				order = new Ginger(order);
+			} else if (beveStr.equals("tea latte")) {
+				order = new RedTea();
+				((TeaBeverage) order).setSize(disArr[i]);
+				order = new Milk(order);
+			} else {
+				System.out.println("Illegal input: " + beveStr);
+				return;
+			}
+
+			for (i++; i < disArr.length; i++) {
+				if (disArr[i].equals("chocolate")) {
+					order = new Chocolate(order);
+				} else if (disArr[i].equals("ginger")) {
+					order = new Ginger(order);
+				} else if (disArr[i].equals("milk")) {
+					order = new Milk(order);
+				} else if (disArr[i].equals("jasmine")) {
+					order = new Jasmine(order);
+				} else if (disArr[i].equals("whip")) {
+					i++;
+					order = new WhipCream(order);
+				} else {
+					System.out.println("Illegal input: " + disArr[i]);
+				}
+			}
+			
+			
+		
+
+		
 		/**
 		 * How do I get the description of each order instead of doing this
 		 * stupid thing forever (except for printing the args)?
@@ -105,6 +122,8 @@ public class Main {
 			((Espresso) order).getDescription();
 		}
 		// and so on...
+		
+		
 
 		DecimalFormat df = new DecimalFormat(".0");
 		System.out.println("The total cost of your order is: "
