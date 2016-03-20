1f3f89304b0a4fb68cbe14ecac306fc9366e22e9
lyz-clarence <14302010020@fudan.edu.cn>
2016-03-20 22:26:03 +0800

    After Merge2.0

--- Lab1/src/lab1/MainTest.java
+++ Lab1/src/lab1/MainTest.java
@@ -18,30 +18,26 @@ public class MainTest {
 		cacheStream = new PrintStream(baoStream);
 		System.setOut(cacheStream);
 	}
-
 	
 	@Test
 	public void testInput1() {
 		String[] str = new String[5];
-		str[0] = "houseblend";
-		str[2] = "whip"; 
-		str[3] = "cream";
-		str[4] = "milk";
+		str[0] = "white";
+		str[1] = "tea"; 
+		str[2] = "small"; 
+		str[3] = "milk";
 		String assertStr = "The total cost of your order is: ";
 		int i = 0;
-		for(i=0;i<=3;i++){
+		for(i=0;i<=2;i++){
 			if(i==0){
-				str[1] = "small"; 
+				str[4] = "milk";
 				assertStr += "1.8";
 			}else if(i==1){
-				str[1] = "medium"; 
+				str[4] = "jasmine"; 
+				assertStr += "2.0";
+			}else {
+				str[4] = "Ginger"; 
 				assertStr += "2.1";
-			}else if(i==2){
-				str[1] = "large"; 
-				assertStr += "2.4";
-			}else{
-				str[1] = "grande";
-				assertStr += "2.7";
 			}
 			Main.main(str);
 			String strMsg = baoStream.toString().trim();
@@ -49,5 +45,28 @@ public class MainTest {
 			baoStream.reset();
 			assertStr = "The total cost of your order is: ";
 		}
-}
+	}
+	
+	@Test
+	public void testInput2() {
+		String[] str = {"2","houseblend","small","milk","chocolate",";","houseblend","small","milk","chocolate"};
+		Main.main(str);
+		String strMsg = baoStream.toString().trim();
+		Assert.assertEquals("The total cost of your order is: 3.6", strMsg);
+		baoStream.reset();	
+		
+		String[] str1 = {"3","houseblend","small","milk","chocolate",";","houseblend","small","milk","chocolate",";","houseblend","small","milk","chocolate"};
+		Main.main(str1);
+		String strMsg1 = baoStream.toString().trim();
+		Assert.assertEquals("The total cost of your order is: 5.4", strMsg1);
+		baoStream.reset();
+		
+		String[] str2 = {"4","houseblend","small","milk","chocolate",";","houseblend","small","milk","chocolate",";","houseblend","small","milk","chocolate",";","houseblend","small","milk","chocolate"};
+		Main.main(str2);
+		String strMsg2 = baoStream.toString().trim();
+		Assert.assertEquals("The total cost of your order is: 7.2", strMsg2);
+		baoStream.reset();
+	
+	}
+	
 }
