839838edbf6e8fb36a910136053eff90d2146e0b
WingCHWang <1147833662@qq.com>
2016-03-20 20:10:53 +0800

    main.java更新

--- Lab1/src/lab1/Main.java
+++ Lab1/src/lab1/Main.java
@@ -26,28 +26,16 @@ public class Main {
 			}
 			
 			case 1: {  //新方式
-				String str = "";
-				for(int j = 1;j < disArr.length; j++) 
-					str += disArr[j]+" ";
-				String[] strArr = str.split(" ; ");
-				if(Integer.parseInt(args[0]) == strArr.length) 
-					for(int j = 0;j < strArr.length;j++) {
-						if(getCostOfOneDrink(strArr[j].split(" ")) == 0)
-							return;
-						else
-							cost += getCostOfOneDrink(strArr[j].split(" "));
-					}
-				else {
-					System.out.println("Beverage number is wrong!");
-					return;
-				}
+				cost = getCostOfSeveralDrink(disArr);
 				break;
 			}
 			
 			
 		}
 		
-
+		if(cost == 0)
+			return;
+		
 		DecimalFormat df = new DecimalFormat(".0");
 		System.out.println("The total cost of your order is: "
 				+ df.format(cost));
@@ -57,94 +45,111 @@ public class Main {
 
 	//计算一杯饮料价格，参数为旧方式的参数  ：<beverage name>  <size>  [<ingredient 1, ingredient 2, ingredient 3>] ，返回double。
 	public static double getCostOfOneDrink(String[] disArr) {
-		
 		//判断商品名字为几位
-		int i;
-		for (i = 0; i < disArr.length; i++)
-			if (disArr[i].equals("small") || disArr[i].equals("medium")
-					|| disArr[i].equals("large") || disArr[i].equals("grande"))  //增加
-				break;
-
-		if (i >= disArr.length) {
-			System.out.println("Must set a size!");
-			return 0;
-		}
-
 		String beveStr;
-		if (i == 2) {
-			beveStr = disArr[0] + " " + disArr[1];
-		} else {
-			beveStr = disArr[0];
-		}
+		Beverage order = new Beverage();
 
-		Beverage order;
-		if (beveStr.equals("espresso")) {
-			order = new CoffeeBeverage();
-			order = new Espresso();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-		} else if (beveStr.equals("houseblend")) {
-			order = new CoffeeBeverage();
-			order = new HouseBlend();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-		} else if (beveStr.equals("mocha")) {
-			order = new Espresso();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-			order = new Chocolate(order);
-		} else if (beveStr.equals("latte")) {
-			order = new Espresso();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-			order = new Milk(order);
-		} else if (beveStr.equals("cappuccino")) {
-			order = new Espresso();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-			order = new WhipCream(order);
-		} else if (beveStr.equals("decaf mocha")) {			//增加
-			order = new Decaf();
-			((CoffeeBeverage) order).setSize(disArr[i]);
-			order = new Chocolate(order);
-		} else if (beveStr.equals("green tea")) {
-			order = new GreenTea();
-			((TeaBeverage) order).setSize(disArr[i]);
-		} else if (beveStr.equals("red tea")) {
-			order = new RedTea();
-			((TeaBeverage) order).setSize(disArr[i]);
-		} else if (beveStr.equals("white tea")) {
-			order = new WhiteTea();
-			((TeaBeverage) order).setSize(disArr[i]);
-		} else if (beveStr.equals("flower tea")) {
-			order = new GreenTea();
-			((TeaBeverage) order).setSize(disArr[i]);
-			order = new Jasmine(order);
-		} else if (beveStr.equals("ginger tea")) {
-			order = new GreenTea();
-			((TeaBeverage) order).setSize(disArr[i]);
-			order = new Ginger(order);
-		} else if (beveStr.equals("tea latte")) {
-			order = new RedTea();
-			((TeaBeverage) order).setSize(disArr[i]);
-			order = new Milk(order);
-		} else {
-			System.out.println("Illegal input: " + beveStr);
-			return 0;
-		}
+			//鍒ゆ柇鍟嗗搧鍚嶅瓧涓哄嚑浣�
+			int i;
+			i_break:
+			for (i = 0; i < disArr.length; i++){
 
-		for (i++; i < disArr.length; i++) {
-			if (disArr[i].equals("chocolate")) {
-				order = new Chocolate(order);
-			} else if (disArr[i].equals("ginger")) {
-				order = new Ginger(order);
-			} else if (disArr[i].equals("milk")) {
-				order = new Milk(order);
-			} else if (disArr[i].equals("jasmine")) {
-				order = new Jasmine(order);
-			} else if (disArr[i].equals("whip")) {
-				i++;
-				order = new WhipCream(order);
-			} else {
-				System.out.println("Illegal input: " + disArr[i]);
-
+				for (int j=0;j<SizeFactor.sizeIndex;j++){
+					if (disArr[i].equals(SizeFactor.sizeDes[j])){
+						break i_break;
+					}				
+				}
 			}
-		}
+				
+
+			if (i >= disArr.length) {
+				System.out.println("Must set a size!");
+				return 0;
+			}
+
+			
+			if (i == 2) {
+				beveStr = disArr[0] + " " + disArr[1];
+			} else {
+				beveStr = disArr[0];
+			}
+
+			
+			if (beveStr.equals("espresso")) {
+				//order = new CoffeeBeverage();
+				order = new Espresso();
+				((CoffeeBeverage) order).setSize(disArr[i]);
+			} else if (beveStr.equals("houseblend")) {
+				//order = new CoffeeBeverage();
+				order = new HouseBlend();
+				((CoffeeBeverage) order).setSize(disArr[i]);
+			} else if (beveStr.equals("mocha")) {
+				order = new Espresso();
+				((CoffeeBeverage) order).setSize(disArr[i]);
+				order = new Chocolate(order);
+			} else if (beveStr.equals("latte")) {
+				order = new Espresso();
+				((CoffeeBeverage) order).setSize(disArr[i]);
+				order = new Milk(order);
+			} else if (beveStr.equals("cappuccino")) {
+				order = new Espresso();
+				((CoffeeBeverage) order).setSize(disArr[i]);
+				order = new WhipCream(order);
+			} else if (beveStr.equals("decaf mocha")) {			//澧炲姞
+				order = new Decaf();
+				((CoffeeBeverage) order).setSize(disArr[i]);
+				order = new Chocolate(order);
+			} else if (beveStr.equals("decaf latte")) {			//澧炲姞
+				order = new Decaf();
+				((CoffeeBeverage) order).setSize(disArr[i]);
+				order = new Milk(order);
+			} else if (beveStr.equals("decaf cappuccino")) {	//澧炲姞
+				order = new Decaf();
+				((CoffeeBeverage) order).setSize(disArr[i]);
+				order = new WhipCream(order);
+			} else if (beveStr.equals("green tea")) {
+				order = new GreenTea();
+				((TeaBeverage) order).setSize(disArr[i]);
+			} else if (beveStr.equals("red tea")) {
+				order = new RedTea();
+				((TeaBeverage) order).setSize(disArr[i]);
+			} else if (beveStr.equals("white tea")) {
+				order = new WhiteTea();
+				((TeaBeverage) order).setSize(disArr[i]);
+			} else if (beveStr.equals("flower tea")) {
+				order = new GreenTea();
+				((TeaBeverage) order).setSize(disArr[i]);
+				order = new Jasmine(order);
+			} else if (beveStr.equals("ginger tea")) {
+				order = new GreenTea();
+				((TeaBeverage) order).setSize(disArr[i]);
+				order = new Ginger(order);
+			} else if (beveStr.equals("tea latte")) {
+				order = new RedTea();
+				((TeaBeverage) order).setSize(disArr[i]);
+				order = new Milk(order);
+			} else {
+				System.out.println("Illegal input: " + beveStr);
+				return 0;
+			}
+
+			for (i++; i < disArr.length; i++) {
+				if (disArr[i].equals("chocolate")) {
+					order = new Chocolate(order);
+				} else if (disArr[i].equals("ginger")) {
+					order = new Ginger(order);
+				} else if (disArr[i].equals("milk")) {
+					order = new Milk(order);
+				} else if (disArr[i].equals("jasmine")) {
+					order = new Jasmine(order);
+				} else if (disArr[i].equals("whip")) {
+					i++;
+					order = new WhipCream(order);
+				} else {
+					System.out.println("Illegal input: " + disArr[i]);
+					return 0;
+				}
+			}
 
 
 		/**
@@ -161,4 +166,26 @@ public class Main {
 
 	}
 	
+	
+	//新方式的价格计算，调用了getCostOfOneDrink()
+	public static double getCostOfSeveralDrink(String[] disArr) {
+		String str = "";
+		double cost = 0;
+		for(int j = 1;j < disArr.length; j++) 
+			str += disArr[j]+" ";
+		String[] strArr = str.split(" ; ");
+		if(Integer.parseInt(disArr[0]) == strArr.length) 
+			for(int j = 0;j < strArr.length;j++) {
+				double cost1 = getCostOfOneDrink(strArr[j].split(" "));
+				if(cost1 == 0)
+					return 0;
+				else
+					cost += cost1;
+			}
+		else {
+			System.out.println("Beverage number is wrong!");
+			return 0;
+		}
+		return cost;
+	}
 }
--- Lab1/src/lab1/MainTest.java
+++ Lab1/src/lab1/MainTest.java
@@ -50,6 +50,28 @@ public class MainTest {
 			baoStream.reset();
 			assertStr = "The total cost of your order is: ";
 		}
-}
+	}
+	
+	@Test
+	public void testInput2() {
+		String[] str = {"2","houseblend","small","milk","chocolate",";","houseblend","small","milk","chocolate"};
+		Main.main(str);
+		String strMsg = baoStream.toString().trim();
+		Assert.assertEquals("The total cost of your order is: 3.6", strMsg);
+		baoStream.reset();	
+		
+		String[] str1 = {"3","houseblend","small","milk","chocolate",";","houseblend","small","milk","chocolate",";","houseblend","small","milk","chocolate"};
+		Main.main(str1);
+		String strMsg1 = baoStream.toString().trim();
+		Assert.assertEquals("The total cost of your order is: 5.4", strMsg1);
+		baoStream.reset();
+		
+		String[] str2 = {"4","houseblend","small","milk","chocolate",";","houseblend","small","milk","chocolate",";","houseblend","small","milk","chocolate",";","houseblend","small","milk","chocolate"};
+		Main.main(str2);
+		String strMsg2 = baoStream.toString().trim();
+		Assert.assertEquals("The total cost of your order is: 7.2", strMsg2);
+		baoStream.reset();
+	
+	}
 
 }
